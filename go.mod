ff1ce965e232888685f7afd0f91e95d681dd2aa2
Shi Han NG <shihanng@gmail.com>
2021-07-16 19:21:11 +0900

    scraper: Add HandleError and go-multierror

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/gocolly/colly/v2 v2.1.0
+	github.com/hashicorp/go-multierror v1.1.1
 	github.com/stretchr/testify v1.7.0
 )
--- scraper/scraper.go
+++ scraper/scraper.go
@@ -4,6 +4,7 @@ import (
 	"net/url"
 
 	"github.com/gocolly/colly/v2"
+	"github.com/hashicorp/go-multierror"
 )
 
 type Scraper struct {
@@ -14,9 +15,17 @@ func NewScraper() *Scraper {
 	return &Scraper{}
 }
 
+func (s *Scraper) HandleError() colly.ErrorCallback {
+	return func(_ *colly.Response, err error) {
+		s.err = multierror.Append(s.err, err)
+	}
+}
+
 func (s *Scraper) SaveHTML() colly.ResponseCallback {
 	return func(r *colly.Response) {
-		s.err = r.Save(urlToFilename(r.Request.URL))
+		if err := r.Save(urlToFilename(r.Request.URL)); err != nil {
+			s.err = multierror.Append(s.err, err)
+		}
 	}
 }
 
